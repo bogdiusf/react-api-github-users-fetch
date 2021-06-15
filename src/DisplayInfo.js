8c0677382703b75c2ba1f9d9666999ded09ccc2a
Filipovici Bogdan <bogdan.filipovici@roreg.eu>
2021-06-15 10:53:37 +0300

    v.2.0

--- src/DisplayInfo.js
+++ src/DisplayInfo.js
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Modal, Button } from 'antd';
+import "antd/dist/antd.css";
+
 
 const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
     > img {
         width: 350px;
         height: 350px;
     }
+    > .deleteButton {
+        width: 50px;
+        height: 30px;
+    }
+    > h3 {
+        font-size: 25px;
+        background-color: rgba(220,220,255,0.9)
+    }
     text-align: center;
+    border: 1px solid black;
+    background-color: rgba(0,255,0,0.5);
+    padding: 40px 40px 20px 40px;
 `;
 
 function DisplayInfo(props) {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+        removeUser(props.id)
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+
+    const removeUser = (id) => {
+        const newArr = props.user.filter((item) => item.id !== id)
+        props.setGit(newArr)
+    }
+
     return (
         <div>
             { (props.state) ? <Wrapper>
+                <Button className="deleteButton" type="danger" onClick={() => showModal()}>X</Button>
                 <img src={props.url} alt="profileImage" />
                 <h3>{props.fullName}</h3>
             </Wrapper>
-            : <div>Cacat</div>}
+                : <div>None</div>}
             {/* {props.msg && <div>Please fetch a valid user</div>} */}
+            <Modal title="Confirmation popup" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <h2>Are you sure you want to remove {props.fullName} from the list?</h2>
+            </Modal>
         </div>
     )
 }
--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,37 +1,105 @@
 import React, { useState } from 'react';
-import './style.css';
 import axios from 'axios';
 import DisplayInfo from './DisplayInfo';
+import "antd/dist/antd.css";
+import './style.css';
+import { Button } from "antd";
+import { Input } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 function MainInfo() {
 
     const [inputUrl, setinputUrl] = useState('')
-    const [git, setGit] = useState(null)
+    const [git, setGit] = useState([])
     const [displayState, setdisplayState] = useState(false)
+
     const url = "https://api.github.com/users/" + inputUrl;
 
+    const [state, setState] = useState(
+        { loadings: [] }
+    )
+    // const fetchData = async () => {
+    //     if (!inputUrl) {
+    //         setdisplayState(false)
+    //         return
+    //     }
 
+    //     try {
+    //         const response = await axios.get(url)
+    //         setGit([...git, response.data])
+    //         setdisplayState(true)
+    //     }
+    //     catch (e) { console.log(e) }
 
-    const getData = async () => {
-        await axios.get(url)
-            .then(response => setGit(response.data), setdisplayState(true), console.log(displayState))
-            .catch(error => console.log(error))
+    //     setinputUrl('')
+    // }
+
+    const enterLoading = (index) => {
+
+        setState(({ loadings }) => {
+            const newLoadings = [...loadings];
+            newLoadings[index] = true;
+            return {
+                loadings: newLoadings
+            };
+
+        });
+
+        setTimeout(async () => {
+
+            if (!inputUrl) {
+                setdisplayState(false)
+                return
+            }
+            else {
+                try {
+                    const response = await axios.get(url)
+                    setGit([...git, response.data])
+                    setdisplayState(true)
+                    
+                    
+                }
+                catch (e) { console.log(e) }
+                
+                setinputUrl('')
+            }
+            setState(({ loadings }) => {
+                const newLoadings = [...loadings];
+                newLoadings[index] = false;
+                
+                return {
+                    loadings: newLoadings
+                };
+            });
+        }, 500);
     };
-
-    // if git has info -> pass git values to fullName and imgUrl, else -> pass empty strings
-    const fullName = git ? git.name : ''; 
-    const imgUrl = git ? git.avatar_url : '';
-
+    
     return (
-        <div>
+        <div className="mainWrapper">
             <div className="header-wrapper">
                 <h1>Fetch GitHub user data</h1>
-                <input type="text" onChange={(event) => { setinputUrl(event.target.value) }} />
-                <button onClick={getData}>Get API data</button>
+                <Input autoSize  placeholder="Type username" prefix={<UserOutlined />} value={inputUrl} onChange={(event) => { setinputUrl(event.target.value) }}
+                style={{width: '100%'}}/>
+                <div style={{ display: 'flex', gap: '20px' }}>
+                    <Button type="primary" loading={state.loadings[0]}
+                        onClick={() => {
+                            if (inputUrl !== '') {
+                                enterLoading(0)
+                            }
+                            else {
+                                alert('Input empty')
+                            }
+                        }}>Get API data</Button>
+                    <Button type="danger" loading={state.loadings[1]} onClick={() => setGit([])}>Clear all users</Button>
+                </div>
             </div>
 
             <div className="wrapper">
-                <DisplayInfo url={imgUrl} fullName={fullName} state={displayState}/>  {/* state -> sending displayState as prop to DisplayInfo.js */}
+                {git.map((item) => (
+                    <div className="penis" key={item.id}>
+                        <DisplayInfo url={item.avatar_url} fullName={item.name} id={item.id} state={displayState} setGit={setGit} user={git}/>
+                    </div>
+                ))}
             </div>
         </div>
     );
